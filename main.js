7326cf5070eb39df25f5da9f09f177830dd60ec9
naitik2009 <89187483+naitik2009@users.noreply.github.com>
2021-10-20 18:52:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function send
+{
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1)*(number2);
+    question_number = "<h4>" + number1 + "X" + number2 + "<h4>";
+    input_box = "<br>Answer : <input type = 'text' id = 'input_check_box'>";
+    check_button = "<br><br><button class = 'btn btn-info' onclick = 'check()'></button>;";
+    document.getElementById("output").innerHTML = row;
+}
